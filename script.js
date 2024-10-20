b447616d3ce8f675f3a393d9d99f5041988d999d
Rob Blake <rob.bl8ke@outlook.com>
2024-10-20 07:43:28 +0200

    Simple element and input selection

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Windows + . to open emoji window...
+
+// text element div, span uses 'textContent'
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Incorrect Number! 🎉';
+console.log(document.querySelector('.message').textContent);
+
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.number').textContent = 13;
+console.log(document.querySelector('.score').textContent);
+document.querySelector('.score').textContent = 10;
+
+// input uses 'value'
+document.querySelector('main section input.guess').value = 15;
+console.log(document.querySelector('main section input.guess').value);
+document.querySelector('input.guess').value = 22;
+console.log(document.querySelector('input.guess').value);
